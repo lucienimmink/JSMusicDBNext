f9f5bf038cb538d20489d93485f96ec8c196907d
Lucien Immink <lucien@arielext.org>
2016-06-17 15:32:25 +0200

    [TASK] Fix offcanvas menu, update settings component

--- app/menu/topmenu.component.ts
+++ app/menu/topmenu.component.ts
@@ -6,34 +6,38 @@ import { Subscription }   from 'rxjs/Subscription';
 @Component({
   templateUrl: 'app/menu/topmenu.component.html',
   selector: 'topmenu',
-  styleUrls: [ 'app/menu/topmenu.component.css' ],
+  styleUrls: ['app/menu/topmenu.component.css'],
   directives: [ROUTER_DIRECTIVES]
 })
 
 export class TopMenuComponent implements OnDestroy {
-    path:string;
-    subscription:Subscription;
-    page:string;
-    subscription2:Subscription;
-    menuVisible:boolean = false;
+  path: string;
+  subscription: Subscription;
+  page: string;
+  subscription2: Subscription;
+  menuVisible: boolean = false;
 
-    constructor(pathService:PathService) {
-      // subscribe to a change in path; so we can display it
-      this.subscription = pathService.pathAnnounced$.subscribe(
-        path => {
-          this.path = path;
-        }
-      );
-      this.subscription2 = pathService.pageAnnounced$.subscribe(
-        page => { this.page = page }
-      );
-    }
+  constructor(pathService: PathService) {
+    // subscribe to a change in path; so we can display it
+    this.subscription = pathService.pathAnnounced$.subscribe(
+      path => {
+        this.path = path;
+        this.menuVisible = false;
+      }
+    );
+    this.subscription2 = pathService.pageAnnounced$.subscribe(
+      page => {
+        this.page = page;
+        this.menuVisible = false;
+      }
+    );
+  }
 
-    ngOnDestroy() {
-      this.subscription.unsubscribe(); // prevent memory leakage
-      this.subscription2.unsubscribe();
-    }
-    toggleMenu() {
-      this.menuVisible = !this.menuVisible;
-    }
+  ngOnDestroy() {
+    this.subscription.unsubscribe(); // prevent memory leakage
+    this.subscription2.unsubscribe();
+  }
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
 }
