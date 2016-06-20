704d77087a32de7c8717638fd14ac4e4e332465a
Lucien Immink <lucien@arielext.org>
2016-06-20 12:33:46 +0200

    [TASK] Add hints to use logic from different project to implement a neat lazy load mechanism

--- app/utils/backgroundart.directive.ts
+++ app/utils/backgroundart.directive.ts
@@ -1,6 +1,9 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { BackgroundArtService } from "./backgroundart.service";
 
+// use logic from https://github.com/NathanWalker/ng2-image-lazy-load/blob/master/src/client/app/directives/image-lazy-load.directive.ts
+// implement workerlogic as well for best performance
+
 const NOIMAGE = 'global/images/no-cover.png';
 
 @Directive({
@@ -9,7 +12,7 @@ const NOIMAGE = 'global/images/no-cover.png';
 })
 export class BackgroundArtDirective implements OnInit {
     private el: HTMLElement;
-    
+
     @Input('backgroundArt') media: any;
 
     constructor(el: ElementRef, private backgroundArtService:BackgroundArtService) {
