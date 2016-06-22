c07c696ad67a937ca28b69d1a7022b03907e4732
Lucien Immink <lucien@arielext.org>
2016-06-22 17:04:52 +0200

    [TASK] Add very simple login page; setting the 'remember me' flag will bypass the login screen next time; though no login handshake is yet in place.

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,18 +1,50 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, REACTIVE_FORM_DIRECTIVES, FormControl, Validators } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   templateUrl: 'app/login/login.component.html',
-  styleUrls: [ 'dist/login/login.component.css']
+  styleUrls: [ 'dist/login/login.component.css'],
+  directives: [ REACTIVE_FORM_DIRECTIVES ],
+  providers: [ LoginService ]
 })
 export class LoginComponent implements OnInit {
   @Input() username:string;
   @Input() password:string;
   @Input() lastfmname:string;
   @Input() dsmport:string;
-  constructor() {}
+  private form: FormGroup;
+  private payLoad:any;
+  private sid;
+
+  constructor(private loginService:LoginService) {
+    let controls:any = {};
+    controls['name'] = new FormControl('', Validators.required);
+    controls['password'] = new FormControl('', Validators.required);
+    controls['dsmport'] = new FormControl('', Validators.required);
+    controls['rememberme'] = new FormControl('');
+    controls['lastfmname'] = new FormControl('');
+    this.form = new FormGroup(controls);
+  }
 
   ngOnInit() {
 
+  }
+  onSubmit() {
+    this.payLoad = JSON.stringify(this.form.value);
+    if (this.form.value.rememberme) {
+      localStorage.setItem('jwt', this.payLoad);
+    }
+    this.loginService.doLogin(this.form.value).subscribe(
+      data => {
+        this.sid = data.data.sid;
+        document.cookie = `id=${this.sid}; path=/`;
+        console.log('session OK; session id set to ', this.sid);
+      },
+      error => {
+        console.error('session failed; bah');
+      }
+    )
   }
 
 };
--- app/main.ts
+++ app/main.ts
@@ -2,20 +2,20 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { enableProdMode, provide } from '@angular/core';
 import { Http, HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { disableDeprecatedForms, provideForms} from '@angular/forms';
 import { IMAGELAZYLOAD_DIRECTIVE } from './utils/imagelazyloadarea.directive';
 import { AuthConfig, AuthHttp } from 'angular2-jwt';
-import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 
 //enableProdMode();
 
 bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, IMAGELAZYLOAD_DIRECTIVE, disableDeprecatedForms()
-   provideForms(), provide(AuthHttp, {
-  useFactory: (http) => {
-    return new AuthHttp(new AuthConfig({
-      tokenName: 'jwt'
-    }), http);
-  },
-  deps: [Http]
-})]);
+   , provideForms(), provide(AuthHttp, {
+    useFactory: (http) => {
+      return new AuthHttp(new AuthConfig({
+        tokenName: 'jwt'
+      }), http);
+    },
+    deps: [Http]
+  })]);
