cc8a4d17fff640433930eb23cb54db0d8779f419
Lucien Immink <lucien@arielext.org>
2016-07-07 14:25:49 +0200

    [TASK] Updated offcanvas menu code

--- app/menu/topmenu.component.ts
+++ app/menu/topmenu.component.ts
@@ -6,58 +6,89 @@ import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import { FormGroup, REACTIVE_FORM_DIRECTIVES, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: 'app/menu/topmenu.component.html',
-  selector: 'topmenu',
-  styleUrls: ['app/menu/topmenu.component.css'],
-  directives: [ROUTER_DIRECTIVES, TOOLTIP_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
+    templateUrl: 'app/menu/topmenu.component.html',
+    selector: 'topmenu',
+    styleUrls: ['app/menu/topmenu.component.css', 'app/menu/side-nav.css'],
+    directives: [ROUTER_DIRECTIVES, TOOLTIP_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
 
 export class TopMenuComponent implements OnDestroy {
-  path: string;
-  subscription: Subscription;
-  page: string;
-  subscription2: Subscription;
-  menuVisible: boolean = false;
-  @Input() query:string;
-  private form: FormGroup;
-  private topSearchVisible: boolean = false;
+    path: string;
+    subscription: Subscription;
+    page: string;
+    subscription2: Subscription;
+    menuVisible: boolean = false;
+    @Input() query: string;
+    private form: FormGroup;
+    private topSearchVisible: boolean = false;
 
-  constructor(private pathService: PathService, private router:Router) {
-    // subscribe to a change in path; so we can display it
-    this.subscription = pathService.pathAnnounced$.subscribe(
-      path => {
-        this.path = path;
-        this.page = null;
+    constructor(private pathService: PathService, private router: Router) {
+        // subscribe to a change in path; so we can display it
+        this.subscription = pathService.pathAnnounced$.subscribe(
+            path => {
+                this.path = path;
+                this.page = null;
+                this.menuVisible = false;
+            }
+        );
+        this.subscription2 = pathService.pageAnnounced$.subscribe(
+            page => {
+                this.page = page;
+                this.path = null;
+                this.menuVisible = false;
+            }
+        );
+
+        let controls: any = {};
+        controls['query'] = new FormControl('');
+        this.form = new FormGroup(controls);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe(); // prevent memory leakage
+        this.subscription2.unsubscribe();
+    }
+    toggleMenu() {
+        this.menuVisible = !this.menuVisible;
+        if (this.menuVisible) {
+            this.slideOut();
+        } else {
+            this.slideIn();
+        }
+    }
+    slideOut() {
+        let sideNavEl = document.querySelector('.js-side-nav');
+        sideNavEl.classList.add('side-nav--animatable');
+        sideNavEl.classList.add('side-nav--visible');
+        document.addEventListener('click', this.onDocumentClick);
+        sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
+    }
+    slideIn() {
+        let sideNavEl = document.querySelector('.js-side-nav');
+        sideNavEl.classList.add('side-nav--animatable');
+        sideNavEl.classList.remove('side-nav--visible');
+        document.removeEventListener('click', this.onDocumentClick);
+        //this.detabinator.inert = true;
+        sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
+    }
+    onTransitionEnd(evt) {
+        let sideNavEl = document.querySelector('.js-side-nav');
+        sideNavEl.classList.remove('side-nav--animatable');
+        sideNavEl.removeEventListener('transitionend', this.onTransitionEnd);
+    }
+    onSubmit() {
+        let query = this.form.value.query;
+        this.topSearchVisible = false;
         this.menuVisible = false;
-      }
-    );
-    this.subscription2 = pathService.pageAnnounced$.subscribe(
-      page => {
-        this.page = page;
-        this.path = null;
-        this.menuVisible = false;
-      }
-    );
-
-    let controls:any = {};
-    controls['query'] = new FormControl('');
-    this.form = new FormGroup(controls);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe(); // prevent memory leakage
-    this.subscription2.unsubscribe();
-  }
-  toggleMenu() {
-    this.menuVisible = !this.menuVisible;
-  }
-  onSubmit() {
-      let query = this.form.value.query;
-      this.topSearchVisible = false;
-      this.menuVisible = false;
-      this.router.navigate(['Search', { query: query }]);
-  }
-  toggleSearch() {
-      this.topSearchVisible = !this.topSearchVisible;
-  }
+        this.router.navigate(['Search', { query: query }]);
+    }
+    toggleSearch() {
+        this.topSearchVisible = !this.topSearchVisible;
+    }
+    private onDocumentClick = (e:Event) => {
+        let target = <HTMLElement>e.target;
+        if (target.classList.contains("side-nav--visible")) {
+            this.toggleMenu();
+        }
+    }
 }
