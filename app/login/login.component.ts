4105c7e5883eb7066bbaf94a91f0dafe4056c69d
Lucien Immink <lucien@arielext.org>
2016-07-08 15:16:42 +0200

    [FIX] Fixed #b3 by triggering a load of the collection after login

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from "@angular/core";
 import { FormGroup, REACTIVE_FORM_DIRECTIVES, FormControl, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { Router } from "@angular/router-deprecated";
+import { CollectionService } from './../collection.service';
+import { CoreService } from './../core.service';
 
 @Component({
   templateUrl: 'app/login/login.component.html',
@@ -18,7 +20,7 @@ export class LoginComponent implements OnInit {
   private payLoad:any;
   private sid;
 
-  constructor(private loginService:LoginService, private router:Router) {
+  constructor(private loginService:LoginService, private router:Router, private collectionService: CollectionService, private coreService:CoreService) {
     let controls:any = {};
     controls['name'] = new FormControl('', Validators.required);
     controls['password'] = new FormControl('', Validators.required);
@@ -36,13 +38,25 @@ export class LoginComponent implements OnInit {
       data => {
         if (data.success) {
           localStorage.setItem('jwt', this.payLoad); // save creds in storage
-          this.router.navigate(['Home']);
+          this.getCollection();
         }
       },
       error => {
         console.error('session failed; bah');
       }
     )
+  }
+
+  getCollection() {
+    this.collectionService.getCollection()
+      .subscribe(
+      data => this.fillCollection(data),
+      error => console.log(error)
+      );
+  }
+  fillCollection(data: any): void {
+    this.coreService.getCore().parseSourceJson(data);
+    this.router.navigate(['Home']);
   }
 
 };
