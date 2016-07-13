30140268dacb7df4beac8364f379e1eb8bb00a89
Lucien Immink <lucien@arielext.org>
2016-07-13 15:27:16 +0200

    [TASK] add google analytics for live error tracking. Updated login component for dark theme support.

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,36 +1,54 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input ,OnDestroy } from "@angular/core";
 import { FormGroup, REACTIVE_FORM_DIRECTIVES, FormControl, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { Router } from "@angular/router-deprecated";
 import { CollectionService } from './../collection.service';
+import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import { CoreService } from './../core.service';
+import { ConfigService } from './../utils/config.service';
+
+import { Subscription }   from 'rxjs/Subscription';
 
 @Component({
   templateUrl: 'app/login/login.component.html',
   styleUrls: [ 'dist/login/login.component.css'],
-  directives: [ REACTIVE_FORM_DIRECTIVES ],
+  directives: [ REACTIVE_FORM_DIRECTIVES, TOOLTIP_DIRECTIVES ],
   providers: [ LoginService ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   @Input() username:string;
   @Input() password:string;
   @Input() lastfmname:string;
   @Input() dsmport:string;
   private form: FormGroup;
   private payLoad:any;
+  private theme:string;
+  private subscription:Subscription;
   private sid;
 
-  constructor(private loginService:LoginService, private router:Router, private collectionService: CollectionService, private coreService:CoreService) {
+  constructor(private loginService:LoginService, private router:Router, private collectionService: CollectionService, private coreService:CoreService, private configService: ConfigService) {
     let controls:any = {};
     controls['name'] = new FormControl('', Validators.required);
     controls['password'] = new FormControl('', Validators.required);
-    controls['dsmport'] = new FormControl('', Validators.required);
+    controls['dsmport'] = new FormControl(document.location.origin, Validators.required);
     controls['lastfmname'] = new FormControl('');
     this.form = new FormGroup(controls);
+
+    this.subscription = this.configService.theme$.subscribe(
+        data => {
+            this.theme = data;
+        }
+    )
+    this.theme = configService.theme;
+
   }
 
   ngOnInit() {
 
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
   onSubmit() {
     this.payLoad = JSON.stringify(this.form.value);
