2077eda44e84e356c953fa2b57828eefd6b77751
Lucien Immink <lucien@arielext.org>
2016-07-18 22:51:16 +0200

    [TASK] Setting up a bundler; not working yet

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp'),
+  tsc = require('gulp-typescript'),
+  Builder = require('systemjs-builder'),
+  inlineNg2Template = require('gulp-inline-ng2-template');
+
+gulp.task('bundle', ['bundle-app', 'bundle-dependencies'], function(){});
+
+gulp.task('inline-templates', function () {
+  return gulp.src('app/**/*.ts')
+    .pipe(inlineNg2Template({ UseRelativePaths: false, indent: 0, removeLineBreaks: true}))
+    .pipe(tsc({
+      "target": "ES5",
+      "module": "system",
+      "moduleResolution": "node",
+      "sourceMap": true,
+      "emitDecoratorMetadata": true,
+      "experimentalDecorators": true,
+      "removeComments": true,
+      "noImplicitAny": false
+    }))
+    .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('bundle-app', ['inline-templates'], function() {
+  // optional constructor options
+  // sets the baseURL and loads the configuration file
+  var builder = new Builder('', 'dist-systemjs.config.js');
+
+  return builder
+    .bundle('dist/app/**/* - [@angular/**/*.js] - [rxjs/**/*.js]', 'bundles/app.bundle.js', { minify: true})
+    .then(function() {
+      console.log('Build complete');
+    })
+    .catch(function(err) {
+      console.log('Build error');
+      console.log(err);
+    });
+});
+
+gulp.task('bundle-dependencies', ['inline-templates'], function() {
+  // optional constructor options
+  // sets the baseURL and loads the configuration file
+  var builder = new Builder('', 'dist-systemjs.config.js');
+
+  return builder
+    .bundle('dist/app/**/*.js - [dist/app/**/*.js]', 'bundles/dependencies.bundle.js', { minify: true})
+    .then(function() {
+      console.log('Build complete');
+    })
+    .catch(function(err) {
+      console.log('Build error');
+      console.log(err);
+    });
+});
