b48ba895eb7a592623be670257ed873b3d3be125
Lucien Immink <lucien@arielext.org>
2016-07-19 14:48:12 +0200

    [TASK] Minified production version is now build in target folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,44 @@
 var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    concat = require('gulp-concat'),
     tsc = require('gulp-typescript'),
     Builder = require('systemjs-builder'),
     inlineNg2Template = require('gulp-inline-ng2-template');
 var tsProject = tsc.createProject('./tsconfig.json');
 
 gulp.task('bundle', ['bundle-app', 'bundle-dependencies'], function () { });
+gulp.task('build', ['bundle', 'copy'], function () { });
+gulp.task('copy', ['copy-css', 'copy-polyfills'], function () { });
 
 gulp.task('inline-templates', function () {
     var tsResult = tsProject.src()
         .pipe(tsc(tsProject))
     return tsResult.js.pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('copy-css', function () {
+    var cssSources = [
+        './node_modules/winstrap/dist/css/winstrap.css',
+        './dist/sass/base.css',
+        './dist/sass/animations.css'
+    ];
+
+    return gulp.src(cssSources)
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./target/css/'));
+});
+
+gulp.task('copy-polyfills', function () {
+    var jsSources = [
+        './node_modules/core-js/client/shim.min.js',
+        './node_modules/zone.js/dist/zone.js',
+        './node_modules/reflect-metadata/Reflect.js',
+        './node_modules/systemjs/dist/system.src.js'
+    ];
+
+    return gulp.src(jsSources)
+        .pipe(concat('polyfills.js'))
+        .pipe(gulp.dest('./target/js/'));
 });
 
 gulp.task('bundle-app', ['inline-templates'], function () {
@@ -18,7 +47,7 @@ gulp.task('bundle-app', ['inline-templates'], function () {
     var builder = new Builder('', 'dist-systemjs.config.js');
 
     return builder
-        .bundle('dist/app/**/* - [@angular/**/*.js] - [rxjs/**/*.js]', 'bundles/app.bundle.js', { minify: true })
+        .bundle('dist/app/**/* - [@angular/**/*.js] - [rxjs/**/*.js]', 'target/js/app.bundle.js', { minify: true })
         .then(function () {
             console.log('Build complete');
         })
@@ -34,7 +63,7 @@ gulp.task('bundle-dependencies', ['inline-templates'], function () {
     var builder = new Builder('', 'dist-systemjs.config.js');
 
     return builder
-        .bundle('dist/app/**/*.js - [dist/app/**/*.js]', 'bundles/dependencies.bundle.js', { minify: true })
+        .bundle('dist/app/**/*.js - [dist/app/**/*.js]', 'target/js/dependencies.bundle.js', { minify: true })
         .then(function () {
             console.log('Build complete');
         })
