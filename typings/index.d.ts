bca8a6c6d4561b9943dc0848e90b8f60ceff3592
Lucien Immink <lucien@arielext.org>
2016-07-31 11:28:07 +0200

    [TASK] Update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,4 +2,5 @@
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/pouchdb/index.d.ts" />
 /// <reference path="globals/pouchdb/pouch/index.d.ts" />
