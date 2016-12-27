62c24f5a5a2337b94e3ed5d2b9f1126a92b2d12b
Lucien Immink <lucien@arielext.org>
2016-12-27 13:26:33 +0100

    [TASK] Bump versions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,3 +4,4 @@
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/pouchdb/index.d.ts" />
 /// <reference path="globals/service_worker_api/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
