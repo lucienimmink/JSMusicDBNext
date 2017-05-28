0587ebff78748f6f15d5cc3ada7184c4ac2c39af
Lucien Immink <lucien@arielext.org>
2017-05-28 21:33:35 +0200

    [TASK] Add hosted webapp functionalities

--- app/player/player.component.ts
+++ app/player/player.component.ts
@@ -44,6 +44,11 @@ export class PlayerComponent implements OnDestroy {
     private forceRestart: boolean = false;
     private showVolumeWindow: boolean = false;
     private volume: number = 100;
+    // onetime check to see if we have a hosted web app
+    private isHostedApp: boolean = (typeof Windows !== 'undefined');
+    private systemMediaControls: any;
+    private displayUpdater: any;
+    c: any;
 
     @ViewChild(AlbumArt) albumart: AlbumArt;
 
@@ -115,7 +120,41 @@ export class PlayerComponent implements OnDestroy {
                 }
             }
         );
-        
+        if (this.isHostedApp) {
+            this.systemMediaControls = Windows.Media.SystemMediaTransportControls.getForCurrentView();
+            this.displayUpdater = this.systemMediaControls.displayUpdater;
+            this.systemMediaControls.isEnabled = true;
+            this.systemMediaControls.isPlayEnabled = true;
+            this.systemMediaControls.isPauseEnabled = true;
+            this.systemMediaControls.isStopEnabled = true;
+            this.systemMediaControls.isNextEnabled = true;
+            this.systemMediaControls.isPreviousEnabled = true;
+            this.systemMediaControls.addEventListener("buttonpressed", (e: Event) => {
+                console.log('systemMediaControlButton is pressed');
+                var mediaButton = Windows.Media.SystemMediaTransportControlsButton;
+                switch (e.button) {
+                    case mediaButton.play:
+                        this.onplay();
+                        break;
+                    case mediaButton.pause:
+                        this.onpause();
+                        break;
+                    case mediaButton.stop:
+                        this.onstop();
+                        break;
+                    case mediaButton.next:
+                        this.next();
+                        break;
+                    case mediaButton.previous:
+                        this.prev();
+                        break;
+                    default:
+                        console.warn('key not mapped in application', e.button);
+                }
+            }, false);
+            this.systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.closed;
+            this.c = this;
+        }
         // not for mobile or Edge atm
         if (navigator.userAgent.indexOf('Mobi') === -1 && navigator.userAgent.indexOf('Edge/') === -1) {
 
@@ -170,6 +209,9 @@ export class PlayerComponent implements OnDestroy {
         }
     }
 
+    systemMediaControlsButtonPressed(e: Event): void {
+
+    }
 
     setTrack(position: any) {
         setTimeout(() => {
@@ -297,6 +339,7 @@ export class PlayerComponent implements OnDestroy {
         if ('mediaSession' in navigator) {
             this.albumartService.getAlbumArt(this.track.trackArtist, this.track.album.name, 'album').subscribe(
                 data => {
+                    // use mediaSession if available
                     navigator.mediaSession.metadata = new MediaMetadata({
                         title: this.track.title,
                         artist: this.track.trackArtist,
@@ -305,12 +348,52 @@ export class PlayerComponent implements OnDestroy {
                             { src: `${data}`, sizes: '500x500', type: 'image/png' }
                         ]
                     });
-                    
+
                     navigator.mediaSession.setActionHandler('play', () => { this.togglePlayPause() });
-                    navigator.mediaSession.setActionHandler('pause',  () => { this.togglePlayPause() });
-                    navigator.mediaSession.setActionHandler('previoustrack',  () => { this.prev() });
-                    navigator.mediaSession.setActionHandler('nexttrack',  () => { this.next() });
-                    
+                    navigator.mediaSession.setActionHandler('pause', () => { this.togglePlayPause() });
+                    navigator.mediaSession.setActionHandler('previoustrack', () => { this.prev() });
+                    navigator.mediaSession.setActionHandler('nexttrack', () => { this.next() });
+
+                }
+            );
+        }
+        if (this.isHostedApp) {
+            this.systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.playing;
+            this.displayUpdater.type = Windows.Media.MediaPlaybackType.music;
+            this.albumartService.getAlbumArt(this.track.trackArtist, this.track.album.name, 'album').subscribe(
+                data => {
+                    // update system transport
+                    try {
+                        if (this.displayUpdater != undefined) {
+                            this.displayUpdater.musicProperties.albumArtist = this.track.trackArtist;
+                            this.displayUpdater.musicProperties.artist = this.track.trackArtist;
+                            this.displayUpdater.musicProperties.albumTitle = this.track.album.name;
+                            this.displayUpdater.musicProperties.title = this.track.title;
+                            if (data) {
+                                this.displayUpdater.thumbnail = Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(new Windows.Foundation.Uri(data));
+                            }
+                            this.displayUpdater.update();
+                        }
+                    } catch (e) {
+                        console.error('error occurred', e);
+                    }
+                    // update live tile
+                    var Notifications = Windows.UI.Notifications;
+                    Notifications.TileUpdateManager.createTileUpdaterForApplication('App').clear();
+                    var tileXml = Notifications.TileUpdateManager.getTemplateContent(Notifications.TileTemplateType.tileSquare150x150PeekImageAndText02);
+                    var textNode = tileXml.getElementsByTagName("text")[0];
+                    textNode.innerText = this.track.title;
+                    textNode = tileXml.getElementsByTagName("text")[1];
+                    textNode.innerText = this.track.trackArtist;
+                    if (data) {
+                        var imageNode = tileXml.getElementsByTagName("image")[0];
+                        imageNode.attributes[1].value = data;
+                    }
+                    var currentTime = new Date();
+                    var expiryTime = new Date(currentTime.getTime() + Number(this.track.duration));
+                    var tileNotification = new Notifications.TileNotification(tileXml);
+                    tileNotification.expirationTime = expiryTime;
+                    Notifications.TileUpdateManager.createTileUpdaterForApplication('App').update(tileNotification);
                 }
             );
         }
@@ -318,9 +401,15 @@ export class PlayerComponent implements OnDestroy {
     }
     onstop() {
         document.querySelector('mdb-player').dispatchEvent(new Event('external.mdbstopped'));
+        if (this.isHostedApp) {
+            this.systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.stopped;
+        }
     }
     onpause() {
         document.querySelector('mdb-player').dispatchEvent(new CustomEvent('external.mdbpaused', { 'detail': this.track }));
+        if (this.isHostedApp) {
+            this.systemMediaControls.playbackStatus = Windows.Media.MediaPlaybackStatus.paused;
+        }
     }
 
     toggleShuffle() {
