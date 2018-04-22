30d7b059e7df1c95ee9cb1328a24b9e2a004dcc9
Lucien Immink <lucien@arielext.org>
2018-04-22 18:10:00 +0200

    [TASK] Use sunset api to determine the auto switch times

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,84 +1,96 @@
-import { Component, OnInit, OnDestroy, Input, AfterViewChecked, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs/Subscription';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  AfterViewChecked,
+  ViewChild
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs/Subscription";
 
-import { PathService } from './../../utils/path.service';
-import { CoreService } from './../../utils/core.service';
-import { CollectionService } from './../../utils/collection.service';
-import { musicdbcore } from './../../org/arielext/musicdb/core';
-import { TimeFormatPipe } from './../../utils/time-format.pipe';
-import { LastfmService } from './../../utils/lastfm.service';
-import { ConfigService } from './../../utils/config.service';
-import { Settings } from './../settings';
+import { PathService } from "./../../utils/path.service";
+import { CoreService } from "./../../utils/core.service";
+import { CollectionService } from "./../../utils/collection.service";
+import { musicdbcore } from "./../../org/arielext/musicdb/core";
+import { TimeFormatPipe } from "./../../utils/time-format.pipe";
+import { LastfmService } from "./../../utils/lastfm.service";
+import { ConfigService } from "./../../utils/config.service";
+import { Settings } from "./../settings";
 
 declare function require(moduleName: string): any;
-const { version: appVersion } = require('../../../../package.json');
-
+const { version: appVersion } = require("../../../../package.json");
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"]
 })
 export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
-
   public totals: any;
   private core: musicdbcore;
   public lastfmusername: string;
-  public connectiontype = 'node-mp3stream'; // we can implement more connection types later on (dsm, local, etc)
-  public connectiondetails = '';
+  public connectiontype = "node-mp3stream"; // we can implement more connection types later on (dsm, local, etc)
+  public connectiondetails = "";
   private subscription: Subscription;
   private subscription2: Subscription;
   private subscription3: Subscription;
   private subscription4: Subscription;
-  public savePlaylistState: boolean = this.booleanState('save-playlist-state');
-  private manualScrobbling: boolean = this.booleanState('manual-scrobble-state');
-  public isContinuesplay: boolean = this.booleanState('continues-play');
-  private smallArt: boolean = this.booleanState('small-art');
-  private manualScrobblingList: Array<any> = JSON.parse(localStorage.getItem('manual-scrobble-list')) || [];
+  public savePlaylistState: boolean = this.booleanState("save-playlist-state");
+  private manualScrobbling: boolean = this.booleanState(
+    "manual-scrobble-state"
+  );
+  public isContinuesplay: boolean = this.booleanState("continues-play");
+  private smallArt: boolean = this.booleanState("small-art");
+  private manualScrobblingList: Array<any> = JSON.parse(
+    localStorage.getItem("manual-scrobble-list")
+  ) || [];
   public isReloading = false;
   public scanperc = 0;
-  public version: String = '__dev__';
+  public version: String = "__dev__";
   public settings: Settings;
   private hasBeenReloading = false;
-  public lastParsed: number = Number(localStorage.getItem('lastParsed'));
+  public lastParsed: number = Number(localStorage.getItem("lastParsed"));
 
-  public visualisation: boolean = this.booleanState('visualisation-state');
-  public preferVideo: boolean = this.booleanState('preferVideo-state');
+  public visualisation: boolean = this.booleanState("visualisation-state");
+  public preferVideo: boolean = this.booleanState("preferVideo-state");
+
+  public startTime: string;
+  public stopTime: string;
 
   private themeForm: NgForm;
-  @ViewChild('themeForm') currentForm: NgForm;
+  @ViewChild("themeForm") currentForm: NgForm;
   private theme: string;
   public mode: string;
 
-  public isVisualCapable: boolean = (navigator.userAgent.indexOf('Mobi') === -1);
+  public isVisualCapable: boolean = navigator.userAgent.indexOf("Mobi") === -1;
 
-  constructor(private pathService: PathService, private coreService: CoreService, private lastFMService: LastfmService,
-    private collectionService: CollectionService, private router: Router, private configService: ConfigService) {
+  constructor(
+    private pathService: PathService,
+    private coreService: CoreService,
+    private lastFMService: LastfmService,
+    private collectionService: CollectionService,
+    private router: Router,
+    private configService: ConfigService
+  ) {
     this.settings = new Settings();
     this.core = this.coreService.getCore();
-    this.subscription = this.core.coreParsed$.subscribe(
-      data => {
-        this.ngOnInit();
-      }
-    );
+    this.subscription = this.core.coreParsed$.subscribe(data => {
+      this.ngOnInit();
+    });
     this.subscription2 = this.lastFMService.manualScrobbleList$.subscribe(
       data => {
         this.manualScrobblingList = data;
       }
     );
-    this.subscription3 = this.configService.theme$.subscribe(
-      data => {
-        this.settings.theme = data;
-        this.theme = data;
-      }
-    );
-    this.subscription4 = this.configService.mode$.subscribe(
-      data => {
-        this.mode = data;
-      }
-    );
+    this.subscription3 = this.configService.theme$.subscribe(data => {
+      this.settings.theme = data;
+      this.theme = data;
+    });
+    this.subscription4 = this.configService.mode$.subscribe(data => {
+      this.mode = data;
+    });
     // setup a form for changing stuff
     this.settings.theme = this.configService.theme;
     this.theme = this.configService.theme;
@@ -92,7 +104,7 @@ export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   private booleanState(key: string): boolean {
     const raw = localStorage.getItem(key);
-    if (raw && raw === 'true') {
+    if (raw && raw === "true") {
       return true;
     }
     return false;
@@ -101,7 +113,9 @@ export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
     this.formChanged();
   }
   formChanged() {
-    if (this.currentForm === this.themeForm) { return; }
+    if (this.currentForm === this.themeForm) {
+      return;
+    }
     this.themeForm = this.currentForm;
     if (this.themeForm) {
       this.themeForm.valueChanges.subscribe(data => {
@@ -114,12 +128,34 @@ export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
   }
 
   ngOnInit() {
-    this.pathService.announcePage('Settings');
+    this.pathService.announcePage("Settings");
     this.totals = this.core.totals;
-    this.lastfmusername = localStorage.getItem('lastfm-username') || '';
-    this.connectiondetails = localStorage.getItem('dsm');
-    this.manualScrobblingList = JSON.parse(localStorage.getItem('manual-scrobble-list'));
-    this.lastParsed = Number(localStorage.getItem('lastParsed'));
+    this.lastfmusername = localStorage.getItem("lastfm-username") || "";
+    this.connectiondetails = localStorage.getItem("dsm");
+    this.manualScrobblingList = JSON.parse(
+      localStorage.getItem("manual-scrobble-list")
+    );
+    this.lastParsed = Number(localStorage.getItem("lastParsed"));
+    this.configService.getSunriseInfo().subscribe(data => {
+      // set this info back in the service
+      this.configService.startHour = this.getHourFromDateString(
+        data.results.sunset
+      );
+      this.configService.stopHour = this.getHourFromDateString(
+        data.results.sunrise
+      );
+      // now use the data from the service
+      this.startTime = this.configService.startTime;
+      this.stopTime = this.configService.stopTime;
+    });
+  }
+
+  private getHourFromDateString(date: string): number {
+    // yyyy-mm-ddThh:mm:ss+offset
+    const d = new Date();
+    const offset = d.getTimezoneOffset() / 60;
+    const baseHour = parseInt(date.split("T")[1].split(":")[0]);
+    return baseHour - offset;
   }
 
   ngOnDestroy() {
@@ -130,103 +166,116 @@ export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
   }
 
   removeLastfm() {
-    localStorage.removeItem('lastfm-username');
-    localStorage.removeItem('lastfm-token');
+    localStorage.removeItem("lastfm-username");
+    localStorage.removeItem("lastfm-token");
     this.ngOnInit();
   }
   removeConnection() {
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('dsm');
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("dsm");
     this.ngOnInit();
   }
   toggleSavePlaylistState() {
     this.savePlaylistState = !this.savePlaylistState;
-    localStorage.setItem('save-playlist-state', this.savePlaylistState.toString());
+    localStorage.setItem(
+      "save-playlist-state",
+      this.savePlaylistState.toString()
+    );
   }
   toggleContinuesPlay() {
     this.isContinuesplay = !this.isContinuesplay;
-    localStorage.setItem('continues-play', this.isContinuesplay.toString());
+    localStorage.setItem("continues-play", this.isContinuesplay.toString());
   }
   toggleManualScrobbling() {
     this.manualScrobbling = !this.manualScrobbling;
-    localStorage.setItem('manual-scrobble-state', this.manualScrobbling.toString());
-    this.manualScrobblingList = JSON.parse(localStorage.getItem('manual-scrobble-list'));
+    localStorage.setItem(
+      "manual-scrobble-state",
+      this.manualScrobbling.toString()
+    );
+    this.manualScrobblingList = JSON.parse(
+      localStorage.getItem("manual-scrobble-list")
+    );
   }
   toggleVisualisation() {
     this.visualisation = !this.visualisation;
     if (!this.visualisation) {
       // small art without visualisation is not needed.
       this.smallArt = false;
-      localStorage.setItem('small-art', this.smallArt.toString());
+      localStorage.setItem("small-art", this.smallArt.toString());
     }
-    localStorage.setItem('visualisation-state', this.visualisation.toString());
+    localStorage.setItem("visualisation-state", this.visualisation.toString());
   }
   togglePreferVideo() {
     this.preferVideo = !this.preferVideo;
-    localStorage.setItem('preferVideo-state', this.preferVideo.toString());
+    localStorage.setItem("preferVideo-state", this.preferVideo.toString());
   }
   toggleSmallArt() {
     this.smallArt = !this.smallArt;
-    localStorage.setItem('small-art', this.smallArt.toString());
+    localStorage.setItem("small-art", this.smallArt.toString());
   }
   scrobbleNow() {
-    this.manualScrobblingList = JSON.parse(localStorage.getItem('manual-scrobble-list'));
-    const track = this.manualScrobblingList.pop();
-    this.lastFMService.scrobbleCachedTrack(track).subscribe(
-      data => {
-        localStorage.setItem('manual-scrobble-list', JSON.stringify(this.manualScrobblingList));
-        if (this.manualScrobblingList.length > 0) {
-          this.scrobbleNow();
-        }
-      }
+    this.manualScrobblingList = JSON.parse(
+      localStorage.getItem("manual-scrobble-list")
     );
+    const track = this.manualScrobblingList.pop();
+    this.lastFMService.scrobbleCachedTrack(track).subscribe(data => {
+      localStorage.setItem(
+        "manual-scrobble-list",
+        JSON.stringify(this.manualScrobblingList)
+      );
+      if (this.manualScrobblingList.length > 0) {
+        this.scrobbleNow();
+      }
+    });
   }
   removeFromScrobbleList(item: any) {
-    this.manualScrobblingList = JSON.parse(localStorage.getItem('manual-scrobble-list'));
+    this.manualScrobblingList = JSON.parse(
+      localStorage.getItem("manual-scrobble-list")
+    );
     let index = -1;
-    this.manualScrobblingList.forEach(function (value, i) {
+    this.manualScrobblingList.forEach(function(value, i) {
       if (Object.is(value, item)) {
         index = i;
       }
     });
     this.manualScrobblingList.splice(index, 1);
-    localStorage.setItem('manual-scrobble-list', JSON.stringify(this.manualScrobblingList));
+    localStorage.setItem(
+      "manual-scrobble-list",
+      JSON.stringify(this.manualScrobblingList)
+    );
   }
   reloadCollection() {
     this.isReloading = true;
-    this.collectionService.reload().subscribe(
-      data => {
+    this.collectionService.reload().subscribe(data => {
+      setTimeout(e => {
+        this.poll();
+      }, 300);
+    });
+  }
+  poll() {
+    this.collectionService.poll().subscribe(data => {
+      this.scanperc = data.progress;
+      if (data.status !== "ready") {
+        this.isReloading = true;
         setTimeout(e => {
           this.poll();
         }, 300);
-      }
-    );
-  }
-  poll() {
-    this.collectionService.poll().subscribe(
-      data => {
-        this.scanperc = data.progress;
-        if (data.status !== 'ready') {
-          this.isReloading = true;
-          setTimeout(e => {
-            this.poll();
-          }, 300);
-          this.hasBeenReloading = true;
-        } else {
-          this.isReloading = false;
-          if (this.hasBeenReloading) {
-            this.getCollection();
-          }
+        this.hasBeenReloading = true;
+      } else {
+        this.isReloading = false;
+        if (this.hasBeenReloading) {
+          this.getCollection();
         }
       }
-    );
+    });
   }
 
   getCollection() {
-    this.collectionService.getCollection()
+    this.collectionService
+      .getCollection()
       .subscribe(
-      data => this.fillCollection(data),
-      error => console.log(error)
+        data => this.fillCollection(data),
+        error => console.log(error)
       );
   }
   fillCollection(data: any): void {
@@ -235,6 +284,6 @@ export class SettingsComponent implements OnInit, OnDestroy, AfterViewChecked {
   }
 
   viewList() {
-    this.router.navigate(['/scrobble-cache']);
+    this.router.navigate(["/scrobble-cache"]);
   }
 }
--- src/app/utils/config.service.ts
+++ src/app/utils/config.service.ts
@@ -1,34 +1,68 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ConfigService {
-  private _theme: string = localStorage.getItem('theme') || 'light';
-  private _mode = 'light';
+  private _theme: string = localStorage.getItem("theme") || "light";
+  private _mode = "light";
   private themeSource = new Subject<any>();
   private modeSource = new Subject<any>();
   private counter: any = 0;
   private COUNTERTIMER: number = 60 * 1000;
   theme$ = this.themeSource.asObservable();
   mode$ = this.modeSource.asObservable();
+  public startHour: number;
+  public stopHour: number;
 
-  constructor() { }
+  constructor(private http: Http) {}
+  public getSunriseInfo(): Observable<any> {
+    return (
+      this.http
+        // would like to get the lat/lng from the browser but don't want to bother the user
+        .get("//api.sunrise-sunset.org/json?lat=51&lng=5&formatted=0")
+        .map(this.getSunriseSunset)
+        .catch(this.handleError)
+    );
+  }
+  private handleError(error: any) {
+    return Observable.throw(null);
+  }
+
+  private getSunriseSunset(res: Response): void {
+    const body = res.json();
+    return body;
+  }
 
   private setStyleSheet(style: string) {
-    const stylesheet = document.createElement('link');
-    stylesheet.setAttribute('rel', 'stylesheet');
-    stylesheet.setAttribute('type', 'text/css');
-    stylesheet.setAttribute('href', (window['ENV'] === 'prod' || !window['ENV']) ? `global/css/${style}.css` : `/dist/sass/${style}.css`);
-    stylesheet.setAttribute('id', 'customStylesheet');
+    const stylesheet = document.createElement("link");
+    stylesheet.setAttribute("rel", "stylesheet");
+    stylesheet.setAttribute("type", "text/css");
+    stylesheet.setAttribute(
+      "href",
+      window["ENV"] === "prod" || !window["ENV"]
+        ? `global/css/${style}.css`
+        : `/dist/sass/${style}.css`
+    );
+    stylesheet.setAttribute("id", "customStylesheet");
 
-    const current = document.getElementById('customStylesheet');
+    const current = document.getElementById("customStylesheet");
     if (current) {
-      document.getElementsByTagName('head')[0].removeChild(current);
+      document.getElementsByTagName("head")[0].removeChild(current);
     }
-    document.getElementsByTagName('head')[0].appendChild(stylesheet);
-    localStorage.setItem('theme', this._theme);
+    document.getElementsByTagName("head")[0].appendChild(stylesheet);
+    localStorage.setItem("theme", this._theme);
     this.themeSource.next(this._theme);
     this.modeSource.next(style);
+  }
+
+  get startTime(): string {
+    return `${this.startHour}:00`;
+  }
+
+  get stopTime(): string {
+    return `${this.stopHour}:00`;
   }
 
   get theme(): string {
@@ -41,10 +75,12 @@ export class ConfigService {
     if (this._theme !== theme) {
       this._theme = theme;
     }
-    if (this._theme === 'auto' && !this.counter) {
-      this.counter = setInterval(() => { this.checkTheme(); }, this.COUNTERTIMER);
+    if (this._theme === "auto" && !this.counter) {
+      this.counter = setInterval(() => {
+        this.checkTheme();
+      }, this.COUNTERTIMER);
       this.checkTheme();
-    } else if (this._theme === 'auto') {
+    } else if (this._theme === "auto") {
       // timer has already started, we can ignore it all!
     } else {
       this.setStyleSheet(this._theme);
@@ -54,9 +90,11 @@ export class ConfigService {
     }
   }
   applyTheme() {
-    if (this._theme === 'auto') {
+    if (this._theme === "auto") {
       if (!this.counter) {
-        this.counter = setInterval(() => { this.checkTheme(); }, this.COUNTERTIMER);
+        this.counter = setInterval(() => {
+          this.checkTheme();
+        }, this.COUNTERTIMER);
       }
       this.checkTheme();
     } else {
@@ -65,12 +103,14 @@ export class ConfigService {
   }
   checkTheme() {
     const d: Date = new Date();
-    if (d.getHours() < 7 || d.getHours() > 20 && this._mode !== 'dark') {
-      this._mode = 'dark';
-      this.setStyleSheet(this._mode);
-    } else if (d.getHours() > 6 && d.getHours() < 21 && this._mode !== 'light') {
-      this._mode = 'light';
-      this.setStyleSheet(this._mode);
+    if (
+      d.getHours() < this.stopHour ||
+      (d.getHours() > this.startHour && this._mode !== "dark")
+    ) {
+      this._mode = "dark";
+    } else if (this._mode !== "light") {
+      this._mode = "light";
     }
+    this.setStyleSheet(this._mode);
   }
 }
