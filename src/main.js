b31066a7acf85be046e3359773fab3d958d1f89f
Lucien Immink <lucien.immink@isaac.local>
2018-10-02 10:46:51 +0200

    [TASK] Add reset CSS and register service-worker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './app.vue';
+import './registerServiceWorker';
+import 'primer-base/index.scss';
 
 Vue.config.productionTip = process.env.NODE_ENV !== 'production';
 
