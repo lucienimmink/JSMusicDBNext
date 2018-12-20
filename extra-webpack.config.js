e3f3f8f4fe0937ebb9f0329687cb6409bf2f69d6
Lucien Immink <lucien@arielext.org>
2018-12-20 13:49:07 +0100

    [TASK] Add purify-css to minimize the CSS payload

---
+++ extra-webpack.config.js
@@ -0,0 +1,18 @@
+const PurifyCSSPlugin = require("purifycss-webpack");
+const path = require("path");
+const glob = require("glob-all");
+
+module.exports = {
+  plugins: [
+    new PurifyCSSPlugin({
+      paths: glob.sync([
+        path.join(__dirname, "src/**/*.html"),
+        path.join(__dirname, "src/**/*.ts")
+      ]),
+      minimize: true,
+      purifyOptions: {
+        whitelist: ["title-bar*", "*tooltip*"]
+      }
+    })
+  ]
+};
